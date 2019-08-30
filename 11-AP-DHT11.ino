26cdf60a0dec3822992f2c230721fd88f4166c20
Luiz Carvalho <carvale71@yahoo.com.br>
2019-08-30 17:05:18 -0300

    Update 11-AP-DHT11.ino

--- 11-AP-DHT11.ino
+++ 11-AP-DHT11.ino
@@ -1,86 +1,45 @@
 /*
-  Conexão Station - WebServer
- IOT na prática com o ESP8266
-  https://github.com/esp8266/Arduino/tree/master/doc/esp8266wifi
-  http://tomeko.net/online_tools/cpp_text_escape.php?lang=en
+  Simple DHT11
+  IOT na prática com o ESP8266
+  https://github.com/winlinvip/SimpleDHT
+  
 */
 
-#include <ESP8266WiFi.h>
+#include <SimpleDHT.h>
 
-// Seu AP da Rede
-const char *ssid = "SEUAP"; //Nome do AP a ser conectado
-const char *password = "SUASENHA"; //Senha do AP
-
-// AP do ESP
-const char *ssid_ap = "SEUNOME"; //Nome do SSID do ESP8266
-const char *password_ap = "pelicano1"; //Senha
-
-
-
-//Configura o WifiServer
-WiFiServer servidor(80);
-
+int pinDHT11 = D1; // Pino onte está conectado o DHT11
+SimpleDHT11 dht11(pinDHT11);
 
 void setup() {
-
-  pinMode(D4, OUTPUT); // Incializa o D4  como saída
-  
   Serial.begin(115200);
-  Serial.println();
-
-//------------ Conectando no AP (STAT) ---------------------
-  WiFi.begin(ssid, password);
-  Serial.print("Conectando");
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println();
-  
-  IPAddress MeuIP = WiFi.localIP();
-  Serial.print("Endereço AP : ");
-  Serial.println(MeuIP);
-//---------------------------------------------------------
-
-  Serial.println();
-  
-//-----------Modo AP--------------------------------------- 
-  Serial.println("Configurando o AP ...");
-  
-  /* Start do AP*/
-  WiFi.softAP(ssid_ap, password_ap);
-  IPAddress MeuIP_ap = WiFi.softAPIP();
-  Serial.print("Endereço AP : ");
-  Serial.println(MeuIP_ap);
-//---------------------------------------------------------
-
-  /*Start WebServer*/
-  servidor.begin();
-
+  pinMode(D4, OUTPUT); // Incializa o D4  como saída
 }
 
 void loop() {
-
-  WiFiClient client = servidor.available();
+  // Incio das Leituras do DHT11
+  Serial.println("=================================");
+  Serial.println("Leitura do DHT11...");
   
-  if (client){
-    
-      Serial.println("Novo Cliente");
-     
-      String pagina = "";
-    
-      /* Página HTML */
- 
-      pagina += "<!doctype html>\n<html lang=\"pt-BR\">\n    <head>\n        <meta charset=\"utf-8\">\n        <title>Curso ESP8266 na Prática</title>\n    </head>\n\n    <body>\n        <h1>Comando Liga/Desliga LED</h1>\n\t\t<p>LED D4\n\t\t\t<a href=?function=d4_on><button>LIGA</button></a>\n\t\t\t<a href=?function=d4_off><button>DESLIGA</button></a>\n\t\t</p>\n\t\t\t\n    </body>\n</html>";
-
-      String req = client.readStringUntil('\r');
-      if (req.indexOf("d4_on") != -1) digitalWrite(D4, LOW);
-      if (req.indexOf("d4_off") != -1) digitalWrite(D4, HIGH);
- 
-      // Envia a página para o browser
-      client.print(pagina);
+  digitalWrite(D4, HIGH); //Pisca
+  delay(500);
+  
+  //Variáveis
+  byte temperatura = 0;
+  byte umidade = 0;
+  
+  int err = SimpleDHTErrSuccess;
+  
+  if ((err = dht11.read(&temperatura, &umidade, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Leitua DHT11 com falha, err="); 
+    Serial.println(err);
+    delay(1000);
+    return;
   }
   
+  Serial.print("Leitura OK: ");
+  Serial.print((int)temperatura); Serial.print(" *C, "); 
+  Serial.print((int)umidade); Serial.println(" %");
+  
+  digitalWrite(D4, LOW);//Pisca
+  delay(500);
 }
